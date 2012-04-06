7f92a69c039a55269633e3881b301d1c2b45e0c2
Kristján Pétursson <kristjan@gmail.com>
2012-04-06 11:43:00 -0700

    Trick JSLint into quiescence
    This little hack avoids the error:
    
        Expected an identifier and instead saw 'static' (a reserved word).

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ app.configure(function() {
     secret: process.env.SESSION_SECRET
   }));
   app.use(everyauth.middleware());
-  app.use(express.static(__dirname + "/../public"));
+  app.use(express['static'](__dirname + "/../public"));
 });
 
 app.configure('development', function() {
