578ce8e32516f9ce4eb2c6c6c390e16df1d47b44
Kristján Pétursson <kristjan@gmail.com>
2012-04-09 14:53:54 -0700

    Style the stages
    Each stage has simple CSS styling with the exception of the header, whichse
    color is taken from the GitHub label. To get an arbitrary number of columns all
    taking up equal space, some JS is responsible for resizing them all as things
    get rendered.

--- public/js/githud.js
+++ public/js/githud.js
@@ -3,10 +3,12 @@ if (typeof GitHUD === 'undefined') GitHUD = {};
 GitHUD.Core = (function() {
   function init() {
     var repo = new GitHUD.Repo('kristjan/playground');
-    repo.labels().fetch({success: loadFlowLabels});
+    repo.labels().fetch({success: loadStages});
+    $(window).resize(resizeUI);
+    resizeUI();
   }
 
-  function loadFlowLabels(labels, models) {
+  function loadStages(labels, models) {
     var flow = _.chain(models)
       .filter(function(model) {
         return model.name.match(/^\d+ - .+/);
@@ -17,11 +19,37 @@ GitHUD.Core = (function() {
     _.each(flow, function(step) {
       printLabel(labels.get(step.name));
     });
+    resizeStages();
   }
 
   function printLabel(label) {
     var view = new GitHUD.StageView({ model: label });
     $('#content').append(view.el);
+  }
+
+  function resizeContent() {
+    var content = $('#content');
+    content.height($('body').height() - content.offset().top - 20);
+  }
+
+  var STAGE_MARGIN = 5;
+  function resizeStages() {
+    var content = $('#content');
+    var stages = $('.stage', content);
+    var spacing = STAGE_MARGIN * (stages.length - 1);
+    var width = content.width() - spacing;
+    stages.css({ width: Math.floor(width / (stages.length)) });
+    stages.not(':last-child').css({ 'margin-right' : STAGE_MARGIN });
+    stages.each(function(i, stage) {
+      stage = $(stage);
+      var list = $('ul', stage);
+      $('ul', stage).height(stage.height() - list.position().top);
+    });
+  }
+
+  function resizeUI() {
+    resizeContent();
+    resizeStages();
   }
 
   return {
